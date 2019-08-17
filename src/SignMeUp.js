23fb46b801077ff094f1276ce4873818c376e836
Ignacio Pastor <ignacio.pastor@clearpeaks.com>
2019-08-17 23:20:59 +0400

    chore: implemented new full code example

---
+++ src/SignMeUp.js
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from 'react';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from 'react-toastify';
+
+const SignMeUp = ({ signupCallback }) => {
+  useEffect(() => {
+    console.log(`SignMeUp: useEffect called`);
+  });
+
+  const [email, setEmail] = useState();
+  const [emailValid, setEmailValid] = useState(false);
+  const [sendProcessing, setSendProcessing] = useState(false);
+
+  function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  }
+
+  const notify = () => {
+    toast.info(`You will be notified of upcoming events ${email}`);
+  };
+
+  async function sendEmailToBackend() {
+    setSendProcessing(true);
+    await new Promise(resolve => {
+      setTimeout(() => {
+        setSendProcessing(false);
+        setEmail('');
+        resolve();
+      }, 1000);
+    });
+    notify();
+    signupCallback(email);
+    setEmail('');
+  }
+
+  const buttonText = sendProcessing ? 'processing...' : 'Get Updates';
+
+  return (
+    <div className="container">
+      <div>
+        <ToastContainer />
+        <div className="content">
+          <input
+            value={email}
+            onChange={e => {
+              setEmailValid(validateEmail(e.target.value));
+              return setEmail(e.target.value);
+            }}
+            placeholder="Enter email"
+            type="email"
+            name="email"
+            required
+          />
+          &nbsp;
+          <button
+            disabled={!emailValid || sendProcessing}
+            className="btn"
+            onClick={sendEmailToBackend}
+            type="submit"
+          >
+            {buttonText}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SignMeUp;
---
+++ src/SpeakerDetail.js
@@ -0,0 +1,35 @@
+import ImageToggleOnMouseOver from './ImageToggleOnMouseOver';
+import ImageToggleOnScroll from './ImageToggleOnScroll';
+
+const SpeakerDetail = React.memo(
+  ({ id, firstName, lastName, favorite, bio, onHeartFavoriteHandler }) => {
+    return (
+      <div className="card col-4 cardmin">
+        <ImageToggleOnScroll
+          className="card-img-top"
+          primaryImg={`/static/speakers/bw/Speaker-${id}.jpg`}
+          secondaryImg={`/static/speakers/Speaker-${id}.jpg`}
+          alt="{firstName} {lastName}"
+        />
+        <div className="card-body">
+          <h4 className="card-title">
+            <button
+              data-sessionId={id}
+              className={favorite ? 'heartredbutton' : 'heartdarkbutton'}
+              onClick={e => {
+                return onHeartFavoriteHandler(e, !favorite);
+              }}
+            />
+            <span>
+              {firstName} {lastName}
+            </span>
+          </h4>
+
+          <span>{bio}</span>
+        </div>
+      </div>
+    );
+  }
+);
+
+export default SpeakerDetail;
